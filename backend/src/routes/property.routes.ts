a8039e586499b4f70aee6dd758d23fa7eacb189f
Devansh Jaiswal <youremail@example.com>
2026-06-07 02:34:29 +0530

    Delete obsolete backend Express folder after complete migration to Next.js

--- backend/src/routes/property.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as propertyController from '../controllers/property.controller';
-
-const router = Router();
-
-router.get('/', propertyController.getProperty);
-router.get('/setup', propertyController.setup);
-
-export default router;
